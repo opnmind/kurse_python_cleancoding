09a42b782fd2df1b698fa79251216eebf2703e25
ucuber <ulrich.cuber@uc-it.de>
2021-03-30 15:15:53 +0200

    Sample

---
+++ Materialien/reader_3.py
@@ -0,0 +1,61 @@
+"""
+Modul IP Finder
+
+Eine Erklärung was heir passiert
+
+Ein Aufrufbeispiel
+
+Verweise
+"""
+import re
+
+
+
+class IpFinder:
+    '''
+    Klassendokumentation
+    '''
+    PATTERN_IP = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+
+    def __init__(self, file_path, pattern_ip=None):
+        """ Docstring """
+        self.file_path = file_path
+        self.log_data = None
+        self.adresses = None
+        self.pattern_ip = pattern_ip if pattern_ip else self.PATTERN_IP
+
+    def read_log(self, file_path=None):
+        '''
+        Funktionsname:
+        '''
+        if file_path:
+            self.file_path = file_path
+        with open(self.file_path, "r") as file_descriptor:
+            self.log_data = file_descriptor.read()
+        return self
+
+    def find_ip_adresses(self, pattern_ip=None):
+        '''
+        Funktionsname:
+        '''
+        if not pattern_ip:
+            pattern_ip = self.pattern_ip
+        matches = re.finditer(pattern_ip, self.log_data, re.MULTILINE)
+        self.adresses = [match.group() for match in matches]
+        return self
+
+
+    def get_ip_adresses(self):
+        '''
+        Funktionsname:
+        '''
+        return self.adresses
+
+if __name__ == "__main__":
+    FILE_PATH = "/home/coder/Workspace/kurse_python_cleancoding/Materialien/Sample.log"
+
+    print(IpFinder(FILE_PATH).
+          read_log().
+          find_ip_adresses().
+          get_ip_adresses())
+    
