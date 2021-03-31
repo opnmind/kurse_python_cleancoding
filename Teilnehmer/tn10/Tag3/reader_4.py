48f9cd3d592976e4e8c67cf8f15b8b58a5348a2d
teilnehmer <teilnehmer@training.uc-it.de>
2021-03-31 13:16:14 +0200

    hugo

---
+++ Teilnehmer/tn10/Tag3/reader_4.py
@@ -0,0 +1,62 @@
+"""
+Modul IP Finder
+
+Eine Erklärung was heir passiert
+
+Ein Aufrufbeispiel
+
+Verweise
+"""
+import typing
+import re
+
+
+
+class IpFinder:
+    '''
+    Klassendokumentation
+    '''
+    PATTERN_IP: str = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+
+    def __init__(self, file_path: str, pattern_ip: str=None) -> None:
+        """ Docstring """
+        self.file_path = file_path
+        self.log_data: str = ""
+        self.adresses: list = []
+        self.pattern_ip = pattern_ip if pattern_ip else self.PATTERN_IP
+
+    def read_log(self, file_path: str=None) -> 'IpFinder':
+        '''
+        Funktionsname:
+        '''
+        if file_path:
+            self.file_path = file_path
+        with open(self.file_path, "r") as file_descriptor:
+            self.log_data = file_descriptor.read()
+        return self
+
+    def find_ip_adresses(self, pattern_ip: str=None) -> 'IpFinder':
+        '''
+        Funktionsname:
+        '''
+        if not pattern_ip:
+            pattern_ip = self.pattern_ip
+        matches = re.finditer(pattern_ip, self.log_data, re.MULTILINE)
+        self.adresses = [match.group() for match in matches]
+        return self
+
+
+    def get_ip_adresses(self) -> list: 
+        '''
+        Funktionsname:
+        '''
+        return self.adresses
+
+if __name__ == "__main__":
+    FILE_PATH = "/home/coder/Workspace/kurse_python_cleancoding/Materialien/Sample.log"
+
+    print(IpFinder(FILE_PATH).
+          read_log().
+          find_ip_adresses().
+          get_ip_adresses())
+    
---
+++ Teilnehmer/tn10/Tag3/reader_4_tests.py
@@ -0,0 +1,28 @@
+import unittest 
+import pytest
+import reader_4
+
+class TestReader(unittest.TestCase):
+    """ TestReader """
+    def test_init(self):
+        pass
+
+    def test_read_log(self):
+        pass
+
+    def test_read_log_fail(self):
+        pass
+
+    def test_find_no_ip_adresses(self):
+        pass
+
+    def test_find_ip_adresses(self):
+        pass
+
+    def test_get_ip_adresses(self):
+        pass
+
+    def test_full_run(self):
+        pass
+
+# if __name__ == '__main__':
