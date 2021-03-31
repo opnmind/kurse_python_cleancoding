4de1d718274ea1e2e74a5bcb72335ad3bc57cfeb
teilnehmer <teilnehmer@training.uc-it.de>
2021-03-31 13:14:09 +0200

    Tag3

---
+++ Teilnehmer/tn09/Tag3/reader_4_test.py
@@ -0,0 +1,19 @@
+import unittest
+import pytest
+import reader_4
+
+class TestReader(unittest.TestCase):
+    def test_init(self):
+        pass
+
+    def test_log_read(self):
+        pass
+
+    def test_find_ip(self):
+        pass
+
+    def test_find_nog_ip(self):
+        pass
+
+    def test_get_ip_adresses(self):
+        pass
